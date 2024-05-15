b6973054a4233b21c9e054c1c06feb8e54d563a7
NoeliaAT <114776463+NoeliaAT@users.noreply.github.com>
2024-05-15 15:41:34 -0300

    ñam ñam

--- Poker.Core/Jugadas/Trio.cs
+++ Poker.Core/Jugadas/Trio.cs
@@ -17,3 +17,23 @@ public class CartaAlta : IJugada
         return new Resultado(Prioridad, valor);
     }
 }
+
+namespace Poker.Core.Jugadas;
+public class CartaAlta : IJugada
+{
+    public string Nombre => "Trio";
+    public byte Prioridad => 7;
+    public Resultado Aplicar(List<Carta> cartas)
+    {
+        if (cartas.Count == 0)
+            throw new ArgumentException("No hay cartas");
+
+        var ordenadasPorValor = cartas.OrderBy(c => c.Valor);
+
+        var valor = ordenadasPorValor.First().Valor == EValor.As ?
+                        (byte)14 : // Aca use el valor 'Alto' del As
+                        (byte)ordenadasPorValor.Last().Valor; // Como estan ordenadas, la ultima es la mayor 
+
+        return new Resultado(Prioridad, valor);
+    }
+}
