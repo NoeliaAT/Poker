0752d3e31c300ae0e4c2e584c13217c8a8d4a87b
NoeliaAT <noelia.almaraz.et12@gmail.com>
2024-05-16 15:07:21 -0300

    ñam ñam

--- Poker.Core/Jugadas/Trio.cs
+++ Poker.Core/Jugadas/Trio.cs
@@ -1,31 +1,9 @@
 namespace Poker.Core.Jugadas;
-public class Trio : IJugada
+
+public class    Trio : IJugada
 {
     public string Nombre => "Trio";
     public byte Prioridad => 7;
-    public Resultado Aplicar(List<Carta> cartas)
-    {
-        if (cartas.Count == 0)
-            throw new ArgumentException("No hay cartas");
-
-        var ordenadasPorValor = cartas.OrderBy(c => c.Valor);
-
-        var valor = ordenadasPorValor.First().Valor == EValor.As ?
-                        (byte)14 : // Aca use el valor 'Alto' del As
-                        (byte)ordenadasPorValor.Last().Valor; // Como estan ordenadas, la ultima es la mayor 
-
-        return new Resultado(Prioridad, valor);
-    }
-}
-
-
-
-namespace Poker12.Core.Jugadas;
-
-public class FullHouse : IJugada
-{
-    public string Nombre => "FullHouse";
-    public byte Prioridad => 4;
 public Resultado Aplicar(List<Carta> cartas)
 {
     if (cartas.Count == 0)
@@ -43,10 +21,6 @@ public Resultado Aplicar(List<Carta> cartas)
             break;
         }
     }
-
-    if (valor != 0 && valorCounts.Count == 2 && valorCounts.ContainsValue(2))
-        return new Resultado(Prioridad, valor);
-    else
         return new Resultado(Prioridad, 0);
 }
 }
