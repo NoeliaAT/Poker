a46715114da3d4d7ba8fc3b7ef87ffc53a7eed7e
NoeliaAT <noelia.almaraz.et12@gmail.com>
2024-05-16 00:19:15 -0300

    ñam ñam

--- Poker.Core/Jugadas/Trio.cs
+++ Poker.Core/Jugadas/Trio.cs
@@ -1,5 +1,5 @@
 namespace Poker.Core.Jugadas;
-public class CartaAlta : IJugada
+public class Trio : IJugada
 {
     public string Nombre => "Trio";
     public byte Prioridad => 7;
@@ -18,22 +18,35 @@ public class CartaAlta : IJugada
     }
 }
 
-namespace Poker.Core.Jugadas;
-public class CartaAlta : IJugada
+
+
+namespace Poker12.Core.Jugadas;
+
+public class FullHouse : IJugada
 {
-    public string Nombre => "Trio";
-    public byte Prioridad => 7;
-    public Resultado Aplicar(List<Carta> cartas)
+    public string Nombre => "FullHouse";
+    public byte Prioridad => 4;
+public Resultado Aplicar(List<Carta> cartas)
+{
+    if (cartas.Count == 0)
+        throw new ArgumentException("No hay cartas");
+
+    var valorCounts = cartas.GroupBy(carta => carta.Valor).ToDictionary(group => group.Key, group => group.Count());
+
+    byte valor = 0;
+
+    foreach (var pair in valorCounts)
     {
-        if (cartas.Count == 0)
-            throw new ArgumentException("No hay cartas");
-
-        var ordenadasPorValor = cartas.OrderBy(c => c.Valor);
-
-        var valor = ordenadasPorValor.First().Valor == EValor.As ?
-                        (byte)14 : // Aca use el valor 'Alto' del As
-                        (byte)ordenadasPorValor.Last().Valor; // Como estan ordenadas, la ultima es la mayor 
-
-        return new Resultado(Prioridad, valor);
+        if (pair.Value == 3)
+        {
+            valor = (byte)pair.Key;
+            break;
+        }
     }
+
+    if (valor != 0 && valorCounts.Count == 2 && valorCounts.ContainsValue(2))
+        return new Resultado(Prioridad, valor);
+    else
+        return new Resultado(Prioridad, 0);
+}
 }
